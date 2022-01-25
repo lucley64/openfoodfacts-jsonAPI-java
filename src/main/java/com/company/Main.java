335ffd10f1bcd197798a0044bdf3fef0df218444
Luc Leydert <luc.leydert@gmail.com>
2022-01-25 20:37:02 +0100

    Mise en place de la recupération du fichier json simplifiée

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,20 +1,19 @@
 package com.company;
 
-import java.io.BufferedInputStream;
 import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
-            BufferedInputStream  inputStream = new BufferedInputStream(new URL("https://world.openfoodfacts.org/api/v0/product/737628064502.json").openStream());
-            FileOutputStream outputStream = new FileOutputStream("737628064502.json");
-            byte dataBuffer[] = new byte[1024];
-            int byteRead;
-            while ((byteRead = inputStream.read(dataBuffer, 0 , 1024)) != -1){
-                outputStream.write(dataBuffer, 0, byteRead);
-            }
+            InputStream inputStream = new URL("https://world.openfoodfacts.org/api/v0/product/737628064502.json").openStream();
+            Files.copy(inputStream, Paths.get("737628064502.json"), StandardCopyOption.REPLACE_EXISTING);
+
         }
         catch (Exception e){
             System.out.println(e.getMessage());
