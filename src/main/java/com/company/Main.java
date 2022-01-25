1135cbfc96adb85d301424a48be5b31c95e5a2b3
Luc Leydert <luc.leydert@gmail.com>
2022-01-25 22:32:12 +0100

    Retourne le nom du produit en fonction d'une URL

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,18 +1,35 @@
 package com.company;
 
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.stream.JsonParser;
+import javax.json.stream.JsonParserFactory;
 import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.io.StringReader;
 import java.net.URL;
+import java.net.URLConnection;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+
         try {
-            InputStream inputStream = new URL("https://world.openfoodfacts.org/api/v0/product/737628064502.json").openStream();
-            Files.copy(inputStream, Paths.get("737628064502.json"), StandardCopyOption.REPLACE_EXISTING);
+            URL url = new URL("https://fr.openfoodfacts.org/api/v0/product/3274080005003.json");
+            URLConnection urlConnection = url.openConnection();
+            urlConnection.setRequestProperty("User-Agent", "ScanEco - Android - Version 0.1");
+            InputStream inputStream = urlConnection.getInputStream();
+            Scanner scanner = new Scanner(inputStream);
+            String json = scanner.useDelimiter("\\A").next();
+            JsonParser parser = Json.createParser(new StringReader(json));
+
+            parser.next();
+
+            System.out.println(parser.getObject().getJsonObject("product").getJsonObject("category_properties").get("ciqual_food_name:fr"));
 
         }
         catch (Exception e){
